096c0bfe7f7e737229b3f713ee316701ca5c5746
Ns2Kracy <2220496937@qq.com>
2022-06-03 22:49:56 +0800

    把以前的删除了，更改一下

--- config/redis.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Redis struct {
-	NetWork string `json:"netWork"`
-	Addr    string `json:"addr"`
-	Port    string `json:"port"`
-	Pwd     string `json:"pwd"`
-	Prefix  string `json:"prefix"`
-}
--- controller/user.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "github.com/gin-gonic/gin"
-
-func Register(ctx *gin.Context) {}
-
-func Login(ctx *gin.Context) {}
-
-func UserInfo(ctx *gin.Context) {}
